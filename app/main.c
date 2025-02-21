f2f43f93b433c1ae85896f99dcbb821b639b76d4
VanLocNguyen1999 <Locnv@selex.vn>
2025-02-21 22:28:02 +0700

    add main_app

---
+++ app/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 21, 2025
+ *      Author: Admin
+ */
+
+#include "board.h"
+
+
+int main(void){
+
+	board_hardware_init_ex();
+	while(1){
+
+	}
+}
---
+++ app/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: Feb 21, 2025
+ *      Author: Admin
+ */
+
+#ifndef APP_MAIN_H_
+#define APP_MAIN_H_
+
+
+
+
+
+#endif /* APP_MAIN_H_ */
