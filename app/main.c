65e965c6f715492a98fe224e4a6b314f16d22038
VanLocNguyen1999 <Locnv@selex.vn>
2025-02-22 01:26:58 +0700

    add

--- app/main.c
+++ app/main.c
@@ -11,6 +11,12 @@
 int main(void){
 
 	board_hardware_init_ex();
+
+	delay_ms(20);
+
+	PWM_TIMER->CCR1 = 0.2*PWM_TIMER_TOP_COUNTER;
+	PWM_ENABLE_CHANNEL;
+	PWM_ENABLE_OUTPUT;
 	while(1){
 
 	}
---
+++ board/Core/Extension/pwm.c
@@ -0,0 +1,38 @@
+/*
+ * pwm.c
+ *
+ *  Created on: Feb 22, 2025
+ *      Author: Admin
+ */
+
+#include "pwm.h"
+
+static void pwm_start(TIM_TypeDef*tim, uint32_t Channel){
+
+	LL_TIM_EnableCounter(tim);
+	LL_TIM_CC_EnableChannel(tim, Channel);
+	LL_TIM_EnableAllOutputs(tim);
+}
+
+static void pwm_set_duty_for_adc_trig(uint32_t duty){
+
+	PWM_TIMER->CCR4 = duty;
+	pwm_start(PWM_TIMER,ADC_TRIG_CHANNEL);
+}
+
+void pwm_init_ex(void){
+
+	pwm_set_frq(1000);
+	PWM_DISABLE_OUTPUT;
+	PWM_STARTUP;
+	PWM_SET_DUTY(0);
+	LL_TIM_EnableCounter(PWM_TIMER);
+}
+void pwm_set_frq(uint32_t frq){
+
+	uint32_t result = 0;
+
+	result = (PWM_FRQ_HZ/frq/2) - 1;
+	PWM_TIMER->ARR = result;
+}
+
